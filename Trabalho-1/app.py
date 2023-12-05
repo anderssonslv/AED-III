7ce810fa484a599e1ad5be2f309a17eeb14ff956
Andersson <89083903+anderssonslv@users.noreply.github.com>
2023-12-05 00:21:17 -0300

    Mudanças
    Organização
    Seleção de Exatos/Aproximativos
    Área de testes (Lab)
    etc...

--- Trabalho-1/app.py
+++ Trabalho-1/app.py
@@ -3,67 +3,73 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import re
 from time import time
-from genetic import geneticAlgorithm
-
-from algoritms.branchAndBound import tsp_branch_and_bound
-from algoritms.bruteForce import tsp_bruteforce
+from algoritms.exact.branchAndBound import tsp_branch_and_bound
+from algoritms.exact.bruteForce import tsp_bruteforce
+from algoritms.prox.genetic import geneticAlgorithm
 
 def getMatrixFromFile(file):
-    try:
-        with open(file, "r") as info:
-            matrix = np.loadtxt(info)
-    except FileNotFoundError:
-        print("Arquivo 'matriz.txt' não encontrado.")
+    with open(file) as matrixFile:
+        matrix = np.loadtxt(matrixFile)
     return matrix
 
-def compairAlgorithm(file, runExact):
+def formatTime(time):
+    mins = int(time // 60)
+    sec = int(time % 60)
+    return f'{mins}m {sec}s'
+
+
+def makeGraph(resultsTable):
+    fig, ax = plt.subplots(figsize=(10, 5))
+    #ax.axis('tight')
+    ax.axis('off')
+    imgTable = ax.table(cellText=resultsTable.values, colLabels=resultsTable.columns, cellLoc='center', loc='center', bbox=[-0.15, 0, 1.2, 1])
+    imgTable.auto_set_font_size(False)
+    imgTable.set_fontsize(10)
+    imgTable.scale(1, 2)
+    return fig
+
+EXACT = False # Rodar exatos ou aproximativos
+
+def compairAlgorithm(file, runTest):
     matrix = getMatrixFromFile(file)
     target = re.search(r'_(\d+)\.', file)
     target = target.group(1)
-    begin = time()
-    totalCostAproximate = geneticAlgorithm(matrix)
-    executeTimeAproximate = time() - begin
-    
-    totalCostExact = 0
-    executeTimeExact = 0
-    
-    if runExact:
-        begin = time()
-        tsp_branch_and_bound(matrix)
-        
-        
+
+    totalCost = 0
+    executeTime = time()
+
+    if EXACT:
+        if runTest:
+            totalCost, pathExat = tsp_branch_and_bound(matrix)
+    else: totalCost = geneticAlgorithm(matrix)
+    executeTime = time() - executeTime
     
     fileName = re.search(r'/(.*?)\.', file)
     fileName = fileName.group(1)
-    return fileName, target,totalCostAproximate, formatTime(executeTimeAproximate), totalCostExact, formatTime(executeTimeExact)
 
-def formatTime(time):
-    
-    mins = int(time // 60)
-    sec = int(time % 60)
-    
-    return f'{mins}m {sec}s'  
+    if EXACT: return fileName, target, totalCost, formatTime(executeTime)
+    if not EXACT: return fileName, target, totalCost, formatTime(executeTime)
 
-matrixFiles = (('tsp_data/tsp1_253.txt', False),
+matrixFiles = (('tsp_data/tsp1_253.txt', True),
                ('tsp_data/tsp2_1248.txt', True),
                ('tsp_data/tsp3_1194.txt', False),
                ('tsp_data/tsp4_7013.txt', False),
                ('tsp_data/tsp5_27603.txt', False))
 
+
 results = []
+for file, runTest in matrixFiles:
+    results.append(compairAlgorithm(file, runTest))
 
-for file, runExact in matrixFiles:
-    results.append(compairAlgorithm(file, runExact))
-
-resultsTable = pd.DataFrame(results, columns=['Arquivo', 'Custo Ótimo', 'Custo\nGenético', 'Tempo de execução\nGenético', 'Custo exato', 'Tempo de execução\nexato'])
-
-fig, ax = plt.subplots(figsize=(10, 5))
-#ax.axis('tight')
-ax.axis('off')
-imgTable = ax.table(cellText=resultsTable.values, colLabels=resultsTable.columns, cellLoc='center', loc='center', bbox=[-0.3, 0, 1.2, 1])
-imgTable.auto_set_font_size(False)
-imgTable.set_fontsize(10)
-imgTable.scale(1, 2)
-
-plt.savefig('algorithmsCompair.png', dpi=200, bbox_inches='tight')
-plt.show()
+if EXACT:
+    resultsTableExact = pd.DataFrame(results, columns=['Arquivo','Custo Ótimo','Custo exato', 'Tempo de execução\nexato'])
+    resultsTable = resultsTableExact
+    fig = makeGraph(resultsTable)
+    plt.savefig('results/exactResults.png', dpi=200, bbox_inches='tight')
+    plt.show()
+else:
+    resultsTableGenetic = pd.DataFrame(results, columns=['Arquivo', 'Custo Ótimo', 'Custo\nGenético', 'Tempo de execução\nGenético'])
+    resultsTable = resultsTableGenetic
+    makeGraph(resultsTable)
+    plt.savefig('results/geneticResults.png', dpi=200, bbox_inches='tight')
+    plt.show()
---
+++ Trabalho-1/lab/bruteForce.py
@@ -0,0 +1,32 @@
+import itertools
+
+def tsp_bruteforce(distances):
+    print("Rodando bruteForce...")
+    num_cities = len(distances)
+    cities = list(range(num_cities))
+    min_distance = float('inf')
+    optimal_path = None
+
+    for path in itertools.permutations(cities):
+        distance = 0
+        for i in range(num_cities - 1):
+            distance += distances[path[i]][path[i+1]]
+        distance += distances[path[-1]][path[0]]  # Volta para a cidade inicial
+
+        if distance < min_distance:
+            min_distance = distance
+            optimal_path = path
+    print(f"Concluido!\nCusto:{min_distance}\nCaminho:{optimal_path}")
+    return min_distance, optimal_path
+
+import numpy as np 
+
+def getMatrixFromFile(file):
+    try:
+        with open(file, "r") as info:
+            matrix = np.loadtxt(info)
+    except FileNotFoundError:
+        print("Arquivo 'matriz.txt' não encontrado.")
+    return matrix
+
+tsp_bruteforce(getMatrixFromFile("tsp1_253.txt"))
